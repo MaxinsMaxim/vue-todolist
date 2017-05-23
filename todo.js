8020d15c1cc46640aa42f00225af5065093ec44b
maxinsMaxim <468091954@qq.com>
2017-05-23 12:37:21 +0800

    'vue-todolist'

---
+++ todo.js
@@ -0,0 +1,93 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        thing: '',
+        todos: [],
+        obj: {},
+        state: '',
+        currentTodo: ''
+    },
+    directives: {
+        // ele: input元素,
+        // bindings.value: currentTodo==todo的结果
+        autoFocus(ele, bindings) {
+            if (bindings.value) {
+                ele.focus();
+            }
+        }
+    },
+    methods: {
+        add() {
+            let obj = {title: this.thing, isChecked: false};
+            this.todos.push(obj);
+            this.thing = '';
+        },
+        remove(todo) {
+            this.todos = this.todos.filter(function (item) {
+                return item != todo;
+            });
+        },
+        changeTitle(todo) {
+            this.currentTodo = todo;
+            for (var key in todo) {
+                if (todo.hasOwnProperty(key)) {
+                    this.obj[key] = todo[key];
+                }
+            }
+        },
+        reset() {
+            this.currentTodo = '';
+        },
+        cancel(todo) {
+            this.todos = this.todos.map(function (item) {
+                if (item == todo) {
+                    return vm.obj;
+                }
+                return item;
+            });
+            this.obj = {};
+            this.currentTodo = '';
+        }
+    },
+    mounted() {
+        this.todos = JSON.parse(localStorage.getItem('todos')) || []; // 如果本地没有数据默认是空数组
+    },
+    watch: {
+        todos: {
+            handler() {
+                // 将todos存到本地 设置key和value, value会被自动toString
+                localStorage.setItem('todos', JSON.stringify(this.todos));
+            },
+            deep: true
+        }
+    },
+    computed: {
+        cloneTodo() {
+            if (this.state == 'complete') { // 全部 返回所有数据
+                return this.todos;
+            }
+            if (this.state == 'finish') {   // 完成 返回checked为true的数据
+                return this.todos.filter(function (item) {
+                    return item.isChecked;
+                });
+            }
+            if (this.state == 'unfinish') { // 未完成
+                return this.todos.filter(function (item) {
+                    return !item.isChecked;
+                });
+            }
+        },
+        total() {
+            return this.todos.filter(function (item) {
+                return !item.isChecked;
+            }).length;
+        }
+    }
+});
+
+let listener = function () {
+    // 当hash值变化时将当前hash值赋予给state
+    vm.state = window.location.hash.slice(1) || 'complete';
+}
+listener();
+window.addEventListener('hashchange', listener, false);
